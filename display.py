5f1cbabb11aee4d16418f58d7b927cdf3e05d22a
lane <lane20@vt.edu>
2025-01-10 19:06:29 -0500

    testing display

--- display.py
+++ display.py
@@ -1,6 +1,7 @@
 from waveshare import epd4in26
 from PIL import Image, ImageDraw, ImageFont
 import time
+import textwrap
 
 epd = epd4in26.EPD()
 
@@ -11,7 +12,11 @@ print('init and clear')
 font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 50)
 image = Image.new('1', (epd.width, epd.height), 255)
 draw = ImageDraw.Draw(image)
-draw.text((10, 10), 'The quick brown fox jumped over the lazy dog to get to the pub to get some cold brews then he went to the at the all for the', font=font, fill=0)
+
+text = 'The quick brown fox jumped over the lazy dog to get to the pub to get some cold brews then he went to the at the all for the'
+wrapped_text = textwrap.fill(text, width=20)  # Adjust width as needed
+
+draw.multiline_text((10, 10), wrapped_text, font=font, fill=0)
 epd.display(epd.getbuffer(image))
 print('displayed')
 time.sleep(5)
