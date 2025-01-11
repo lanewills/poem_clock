c33f1aabc0f9c55a52a04345a0fe4d525a958033
lane <lane20@vt.edu>
2025-01-10 19:10:44 -0500

    testing display

--- display.py
+++ display.py
@@ -14,7 +14,7 @@ image = Image.new('1', (epd.width, epd.height), 255)
 draw = ImageDraw.Draw(image)
 
 text = 'The quick brown fox jumped over the lazy dog to get to the pub to get some cold brews then he went to the at the all for the'
-wrapped_text = textwrap.fill(text, width=20)  # Adjust width as needed
+wrapped_text = textwrap.fill(text, width=25)  # Adjust width as needed
 
 draw.multiline_text((10, 10), wrapped_text, font=font, fill=0)
 epd.display(epd.getbuffer(image))
